f4a24f57273be4fff2e0dbe0953f81deaee13dbc
unknown <you@example.com>
2025-08-26 13:59:32 -0400

    Testes

--- src/main/java/dev/java10x/CadastroDeNinjas/teste.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/teste.java
@@ -2,6 +2,6 @@ package dev.java10x.CadastroDeNinjas;
 
 public class teste {
     public static void main(String[] args) {
-        System.out.println("Fazendo testes");
+        System.out.println("Fazendo teste");
     }
 }
